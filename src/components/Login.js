df1a49e8b41b9597cdb808bde61df3be2cce1ae7
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-12-20 13:15:32 -0800

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [showSignInForm, setShowSignInForm] = useState(true);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
   const toggleForm = () => {
     setShowSignInForm(!showSignInForm);
   };
+
+  const handleButtonClick = (e) => {
+    // Validate the form data
+    e.preventDefault();
+    console.log(email.current.value, password.current.value);
+    console.log(checkValidData(email.current.value, password.current.value));
+  };
+
   return (
     <div>
       <Header />
@@ -14,13 +27,14 @@ const Login = () => {
         alt="netflix-bg"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/00a87542-9fca-4fe9-911f-14544571a547/CA-en-20231211-popsignuptwoweeks-perspective_alpha_website_large.jpg"
       />
-      <div className="h-screen relative flex justify-center top-24 ">
+      <div className="relative flex justify-center top-24 ">
         <div className="p-12 bg-black z-40 w-4/12 bg-opacity-80 rounded-sm">
           <h1 className="text-3xl text-white m-2 mb-5">
             {showSignInForm ? "Sign In" : "Sign Up"}
           </h1>
           <form className="flex flex-col ">
             <input
+              ref={email}
               type="text"
               placeholder="Email or phone number"
               className="p-3 m-2 rounded-sm bg-input-gray text-white"
@@ -33,11 +47,15 @@ const Login = () => {
               />
             )}
             <input
+              ref={password}
               type="password"
               placeholder="Password"
               className="p-3 m-2 rounded-sm bg-input-gray text-white"
             />
-            <button className="bg-red-500 rounded-sm text-white m-2 p-2 mt-5">
+            <button
+              onClick={(e) => handleButtonClick(e)}
+              className="bg-red-500 rounded-sm text-white m-2 p-2 mt-5"
+            >
               Sign In
             </button>
           </form>
---
+++ src/utils/validate.js
@@ -0,0 +1,12 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(
+    email
+  );
+  const isPassValid =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPassValid) return "Password is not valid";
+
+  return null;
+};
